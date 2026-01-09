5563be03abf2403ad0996a76966bbe4461291312
harry774 <xylo.hardik@gmail.com>
2026-01-09 16:02:49 -0500

    updated deploy.yml

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -29,10 +29,6 @@ const Contact = () => {
     }
     setLoading(true);
     try {
-      console.log("emailJS env", {
-        SERVICE_ID,
-        TEMPLATE_ID,
-        PUBLIC_KEY,});
       await emailjs.send(
         SERVICE_ID,
         TEMPLATE_ID,
